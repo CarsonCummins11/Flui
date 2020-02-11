26b460ce9ace58addd0ddc263b08601a56a291c3
Carson Cummins <carsoncummins11@gmail.com>
2020-02-11 09:08:19 -0800

    added more secrets

--- scraping/twitter.py
+++ scraping/twitter.py
@@ -10,6 +10,8 @@ API secret key:TVet3ZZ68Qn3rtDl6IbdsmbV1pIhpuSaFHM4GCPxCZF9HS8Ffs
 #auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 #auth.set_access_token(access_token, access_token_secret)
 
+access_token ->1225240357387956225-TMEOZx7mWGHBLUNOA0EQgEwdNEOr0l
+access_secret -> qsQGjoUownO2l21qx98qjEqtJt8T6IARP3p8d3lA8JaP6
 #api = tweepy.API(auth)
 '''
 api = None
