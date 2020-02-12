3a0bd92fab07ab98b9ec3163fcdc6b54dbaccb81
Carson Cummins <carsoncummins11@gmail.com>
2020-02-11 19:33:55 -0800

    moved setup into one folder

--- setup_windows.bat
+++
@@ -1,6 +0,0 @@
-pip install -r requirements.txt
-bitsadmin /transfer mongodbdownloadjob /download /priority normal https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2012plus-4.2.3-signed.msi
-msiexec.exe /l*v mdbinstall.log  /qb /i mongodb-win32-x86_64-2012plus-4.2.3-signed.msi
-mongod.exe --service
-python dbsetup.py
-python3 -m flask run
