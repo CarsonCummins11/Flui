09e3dbd3d4d048457f3b5c83567059f9d37ade66
Andrew Chester <39309332+andrewchester@users.noreply.github.com>
2020-02-13 08:54:13 -0800

    Created set_engagement_ratio()

--- app/scraping/instagram.py
+++ app/scraping/instagram.py
@@ -7,20 +7,23 @@ from instagram_private_api import Client, ClientCompatPatch, ClientError, Client
 class InstagramBot:
 	#Log into instagram and init api object with auth data
 	def __init__(self):
-		self.api = Client(autopatch = True, authenticate = True, username = auth_data['insta_auth']['username'], password = auth_data['insta_auth']['username'])
+		self.api = Client(autopatch = True, authenticate = True, username = auth_data['insta_auth']['username'], password = auth_data['insta_auth']['password'])
 		self.user_id = auth_data['insta_auth']["id"]
 		self.potential_influencers = []
 		self.indata = {}
+		self.engagement_score = 0
 
 	#calculate the engagement ratio for a specific user
 	def set_engagement_ratio(self, username):
 		posts = self.api.username_feed(username)
 		info = self.api.username_info(username)
-		
 		print(info)
+				
+		ratios = []
+		for post in posts['items']:
+			ratios.append(post['like_count'] / info['follower_count']
+		self.engagement_score = sum(ratios) / len(ratios)			
 
-#testing commands, remove when finished		
-print("Initializing instagram bot")
+#testing commands, remove when finished
 insta_bot = InstagramBot()
 insta_bot.set_engagement_ratio(auth_data['insta_auth']['username'])
-	
