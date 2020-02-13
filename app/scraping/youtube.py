f777395877c4c6142defda17a981a872377759b7
Andrew Chester <39309332+andrewchester@users.noreply.github.com>
2020-02-13 09:12:00 -0800

    Update youtube.py

--- app/scraping/youtube.py
+++ app/scraping/youtube.py
@@ -1,3 +1,6 @@
 import youtube-python
 from app.scraping.authentication.auth import auth_data
 
+class YoutubeBot:
+	def __init__(self):
+		self.api = API(client_id='', client_secret='', api_key='', access_token='optional')
